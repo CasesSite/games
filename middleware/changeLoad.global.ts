7ff00fbd7913b867f4ecebc4f7e3b632377dc2b5
Knara <73983538+knarakochkanian@users.noreply.github.com>
2024-12-02 17:38:40 +0400

    (fix) protect routh

--- middleware/changeLoad.global.ts
+++ middleware/changeLoad.global.ts
@@ -1,6 +1,12 @@
-import { defineNuxtRouteMiddleware, useNuxtApp } from "#app";
-import { useGlobalStore, useGlobalStoreRefs } from "~/stores/useGlobalStore";
+import { defineNuxtRouteMiddleware } from "#app";
+import { useGlobalStore } from "~/stores/useGlobalStore";
 
-export default defineNuxtRouteMiddleware(() => {
-  const { load } = useGlobalStoreRefs();
+export default defineNuxtRouteMiddleware(async (to) => {
+  const globalStore = useGlobalStore();
+
+  globalStore.checkAuthorization();
+
+  if (to.path === "/account/inventory" && !globalStore.isAuthorizedUser) {
+    return navigateTo("/not-authorized");
+  }
 });
