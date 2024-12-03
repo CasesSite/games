1d5a4d1ae7852c3d86b343c04c75756835848a9b
Knara <73983538+knarakochkanian@users.noreply.github.com>
2024-12-03 08:56:37 +0400

    (fix) redirect fix in global middleware

--- middleware/changeLoad.global.ts
+++ middleware/changeLoad.global.ts
@@ -1,12 +1,26 @@
-import { defineNuxtRouteMiddleware } from "#app";
-import { useGlobalStore } from "~/stores/useGlobalStore";
+import { useGlobalStore, useGlobalStoreRefs } from "@/stores/useGlobalStore";
+import { useRouter, useCookie, navigateTo } from "#app";
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const globalStore = useGlobalStore();
+  const router = useRouter();
+  const tokenCookie = useCookie("AccessToken");
+  const { isAuthorizedUser } = useGlobalStoreRefs();
 
-  globalStore.checkAuthorization();
-
-  if (to.path === "/account/inventory" && !globalStore.isAuthorizedUser) {
+  if (to.path === "/account/inventory" && !isAuthorizedUser.value) {
     return navigateTo("/not-authorized");
+  }
+  if (to.path === "/not-authorized" && isAuthorizedUser.value) {
+    return navigateTo("/");
+  }
+
+  try {
+    await userService.getSelf();
+  } catch (error) {
+    if (error.response?.status === 401) {
+      handleUnauthorizedUser(globalStore, tokenCookie, router);
+    } else {
+      console.error("An unexpected error occurred:", error);
+    }
   }
 });
